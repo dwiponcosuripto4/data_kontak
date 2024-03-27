74760492c5c5865cc73dbfeff52f5a971a089c4c
Dwiponco Suripto <dwiponcosuripto44@gmail.com>
2024-03-27 12:36:31 +0700

    add Kontak Controller

--- lib/controller/KontakController.dart
+++ lib/controller/KontakController.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:data_kontak/class/kontak.dart';
@@ -6,5 +7,44 @@ import 'package:data_kontak/service/KontakService.dart';
 class KontakController {
   final kontakService = KontakService();
 
-  
+  Future<Map<String, dynamic>> addPerson(Kontak person, File? file) async {
+    Map<String, String> data = {
+      'nama': person.nama,
+      'email': person.email,
+      'alamat': person.alamat,
+      'no_telepon': person.noTelepon,
+    };
+
+    try {
+      var response = await kontakService.addPerson(data, file);
+      if (response.statusCode == 201) {
+        return {
+          'success': true,
+          'message': 'Data berhasil disimpan',
+        };
+      } else {
+        // Penanganan ketika Content-Type bukan application/json
+        if (response.headers['content-type']!.contains('application/json')) {
+          var decodedJson = jsonDecode(response.body);
+          return {
+            'success': false,
+            'message': decodedJson['message'] ?? 'Terjadi kesalahan',
+          };
+        }
+
+        var decodedJson = jsonDecode(response.body);
+        return {
+          'success': false,
+          'message':
+              decodedJson['message'] ?? 'Terjadi kesalahan saat menyimpan data',
+        };
+      }
+    } catch (e) {
+      // Menangkap kesalahan jaringan atau saat decoding JSON
+      return {
+        'success': false,
+        'message': 'Terjadi kesalahan: $e',
+      };
+    }
+  }
 }
